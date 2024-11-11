b1c954eb5c29cf73fa23c58db72cf1f10ff872c4
Abdellah <abdallahkaba98@gmail.com>
2024-11-11 00:06:59 +0000

    ProductDTO and ProductMapper

--- src/main/java/com/isi/mvc/repository/ProductRepository.java
+++ src/main/java/com/isi/mvc/repository/ProductRepository.java
@@ -1,9 +1,8 @@
-package com.isi.mvc.model.repository;
+package com.isi.mvc.repository;
 
 import com.isi.mvc.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
