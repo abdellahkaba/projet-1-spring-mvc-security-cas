491e16d636b46fac3924c7879f40c68daaa73766
Abdellah <abdallahkaba98@gmail.com>
2024-11-14 13:51:39 +0000

    Pushing to docker hub 1

--- src/test/java/com/isi/mvc/SpringMvc1ApplicationTests.java
+++ src/test/java/com/isi/mvc/SpringMvc1ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.isi.mvc;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringMvc1ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.isi.mvc;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringMvc1ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
